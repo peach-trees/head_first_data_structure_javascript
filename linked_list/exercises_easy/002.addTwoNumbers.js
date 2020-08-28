f9512214c8e5e376ac739873be0fe82a4f3813e0
zary <hanchao@geetest.com>
2020-08-28 17:01:08 +0800

    fixed(002): format code

--- linked_list/exercises_easy/002.addTwoNumbers.js
+++ linked_list/exercises_easy/002.addTwoNumbers.js
@@ -1,32 +1,26 @@
-const Node = require('../linked_list').Node
+const { Node } = require("../linked_list");
 
-function addTwoNumbers(l1, l2) {
-  if (l1 === null || l2 === null) {
-    return null
-  } //if>
+const addTwoNumbers = (l1, l2) => {
+  if (l1 === null || l2 === null) return null;
 
-  const sentry = new Node(0)
-  let cur = sentry
-  let carry = 0
-  while (l1 !== null || l2 !== null || carry > 0) {
-    let curSum = carry
-    if (l1 !== null) {
-      curSum += l1.value
-    } //if>>
-    if (l2 != null) {
-      curSum += l2.value
-    } //if>>
+  const head = new Node(0);
+  let [cur, carry] = [head, 0];
 
-    cur.next = new Node(Math.floor(curSum % 10))
-    cur = cur.next
-    carry = curSum >= 10 ? 1 : 0
-    if (l1 !== null) {
-      l1 = l1.next
-    } //if>>
-    if (l2 !== null) {
-      l2 = l2.next
-    } //if>>
-  } //while>
+  while (l1 || l2 || carry) {
+    let curSum = carry;
+    if (!!l1) {
+      curSum += l1.value;
+      l1 = l1.next;
+    }
+    if (!!l2) {
+      curSum += l2.value;
+      l2 = l2.next;
+    }
 
-  return sentry.next
-}
+    cur.next = new Node(curSum % 10);
+    cur = cur.next;
+    carry = curSum >= 10 ? 1 : 0;
+  }
+
+  return head.next;
+};
--- linked_list/linked_list.js
+++ linked_list/linked_list.js
@@ -1,12 +1,12 @@
 class Node {
   constructor(value) {
-    this.value = value
-    this.next = null
+    this.value = value;
+    this.next = null;
   }
 
   string() {
-    return `[value:${this.value}]`
+    return `[value:${this.value}]`;
   }
 }
 
-exports.Node = Node
+exports.Node = Node;
